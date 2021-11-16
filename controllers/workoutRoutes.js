e9dba7b6804b085fbe84e80cb3ef5cf21b949aae
Yi Liu <elenaliu0415@gmail.com>
2021-11-16 15:08:45 -0800

    Fixed route typo

--- controllers/workoutRoutes.js
+++ controllers/workoutRoutes.js
@@ -1,37 +1,36 @@
-const router = require('express').Router();
-const Workout = require('../models/workout.js');
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
-
-router.get('/', async (req, res) => {
-    // const result = Workout.aggregate([
-    //     {
-    //         "$addFields": {
-    //             "totalDuration": { "$sum": "$exercises.duration" }
-    //         }
-    //     }
-    // ]);
-    const workouts = await Workout.find({});
-    // console.log(result);
-    // console.log(workouts);
-    res.json(workouts);
+router.get("/", async (req, res) => {
+//   const result = await Workout.aggregate([
+//     {
+//       $addFields: {
+//         totalDuration: { $sum: "$exercises.duration" },
+//       },
+//     },
+//   ]);
+  // const workouts = await Workout.find({});
+//   console.log(result);
+  // console.log(workouts);
 });
 
-router.get('/range', async (req, res) => {
-    const workouts = await Workout.find({});
-    res.json(workouts);
+router.get("/range", async (req, res) => {
+  const workouts = await Workout.find({});
+  res.json(workouts);
 });
 
 router.put("/:id", async (req, res) => {
-    let workout = await Workout.findById(req.params.id);
+  let workout = await Workout.findById(req.params.id);
+  // console.log(req.body);
+  workout.exercises.push(req.body);
 
-    workout.excercises.push(req.body);
-    const result = await workout.save();
-    res.json(result);
+  const result = await workout.save();
+  res.json(result);
 });
 
 router.post("/", async (req, res) => {
-    const workout = await Workout.create({ day: new Date() });
-    res.json(workout);
+  const workout = await Workout.create({ day: new Date() });
+  res.json(workout);
 });
 
 module.exports = router;
